d35eb67f4a5616f6f1833e07edefc7aa6e93b40d
priya-kadolkar <priyakadolkar95@gmail.com>
2022-04-26 10:46:42 +0530

    My second springboot application

---
+++ src/main/java/com/dilithe/retailapp/RetailAppApplication.java
@@ -0,0 +1,13 @@
+package com.dilithe.retailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilithe/retailapp/RetailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilithe.retailapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
